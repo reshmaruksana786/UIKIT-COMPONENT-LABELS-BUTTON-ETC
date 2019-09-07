3fb2258c132007d36db497197c1d71bb2790ce8c
Syed.Reshma Ruksana <reshmasmile468@achiever-107.local>
2019-09-07 10:26:21 +0530

    Initial Commit

---
+++ UIKIT COMPONENT BUTTON BY DRAG AND DROP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKIT COMPONENT BUTTON BY DRAG AND DROP
+//
+//  Created by Syed.Reshma Ruksana on 9/7/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
